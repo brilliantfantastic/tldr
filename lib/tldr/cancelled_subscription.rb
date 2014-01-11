cf58a5059fc750d2e7f0e0377373c0b76069b407
Jamie Wright <jamie@brilliantfantastic.com>
2014-01-11 17:59:17 -0500

    attr_accessible for cancelled subscriptions

--- lib/tldr/cancelled_subscription.rb
+++ lib/tldr/cancelled_subscription.rb
@@ -2,5 +2,6 @@ require 'active_record'
 
 module Tldr
   class CancelledSubscription < ActiveRecord::Base
+    attr_accessible :subscriber_id, :email_name
   end
 end
--- lib/tldr/version.rb
+++ lib/tldr/version.rb
@@ -1,3 +1,3 @@
 module Tldr
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
