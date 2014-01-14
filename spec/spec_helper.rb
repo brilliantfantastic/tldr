c94d1e2ab58fef72d8bec740152f93ae22cd70d2
Jamie Wright <jamie@brilliantfantastic.com>
2014-01-14 16:23:27 -0500

    Moved coveralls call to the top of the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'mocha/setup'
-require 'coveralls'
 
 require_relative '../lib/tldr'
-
-Coveralls.wear!
